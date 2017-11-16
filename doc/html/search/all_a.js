c0fc2680c8eb09ff3f36e49529d53eaf8ddbb707
Jan Atle Ramsli <jaramsli@gmail.com>
2017-11-16 18:06:55 +0100

    Added some documentation

---
+++ doc/html/search/all_a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['v',['v',['../structmatrix.html#a18edff6f6f8cc7071adfd9bc826378ef',1,'matrix']]]
+];
---
+++ doc/html/structlinkage.js
@@ -0,0 +1,5 @@
+var structlinkage =
+[
+    [ "base_x", "structlinkage.html#ab7212a9ff1ec58211f1b689374f11766", null ],
+    [ "base_y", "structlinkage.html#aed760843bedfbb6de7f205758ef0d49b", null ]
+];
